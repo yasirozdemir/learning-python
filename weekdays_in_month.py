5aa1b63a5035c53f5ef19687d824337957827a4e
M Yasir Ozdemir <muhammedyasirozdemircareer@gmail.com>
2023-09-21 09:48:09 +0300

    weekdays_in_month_2

--- weekdays_in_month.py
+++ weekdays_in_month.py
@@ -6,10 +6,34 @@ def weekdays_in_month(
 ):  # week_day -> 0: Monday, 1: Tuesday, ..., 6: Sunday
     all_days_in_month = calendar.Calendar().itermonthdays(year, month)
 
-    count_days = 0
+    day_count = 0
 
     for day in all_days_in_month:
         if day != 0 and calendar.weekday(year, month, day) == week_day:
-            count_days += 1
+            day_count += 1
 
-    return count_days
+    return day_count
+
+
+def weekdays_in_month_2(year, month, week_day):
+    weeks_list = calendar.monthcalendar(year, month)
+    # monthcalendar returns an array of week lists
+    # ex. Aug 2022
+    # [
+    #     [1, 1, 1, 1, 1, 1, 1],
+    #     [1, 1, 1, 1, 1, 1, 1],
+    #     [1, 1, 1, 1, 1, 1, 1],
+    #     [1, 1, 1, 1, 1, 1, 1],
+    #     [1, 1, 1, 0, 0, 0, 0],
+    # ]
+
+    day_count = 0
+
+    for week in weeks_list:
+        if week[week_day] != 0:
+            day_count += 1
+    return day_count
+
+
+print(weekdays_in_month(2022, 8, 2))
+print(weekdays_in_month_2(2022, 8, 2))
